21cac1029a4120523697444f656268a835c94b26
grek2 <madisonwharton0@gmail.com>
2025-09-19 15:21:49 -0400

    i have nothing to do so im recreating the entire codebase from scratch

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subsystems/MovementSubsystem.java
@@ -0,0 +1,14 @@
+package org.firstinspires.ftc.teamcode.Subsystems;
+
+import com.arcrobotics.ftclib.command.SubsystemBase;
+import com.arcrobotics.ftclib.hardware.motors.Motor;
+
+public class MovementSubsystem extends SubsystemBase {
+
+    public Motor frontLeftMotor;
+    public Motor frontRightMotor;
+    public Motor backLeftMotor;
+    public Motor backRightMotor;
+
+
+}
