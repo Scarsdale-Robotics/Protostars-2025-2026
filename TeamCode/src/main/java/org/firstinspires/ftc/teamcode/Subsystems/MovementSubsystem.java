380ae584cfd5dd8d25c394a49c93f8a41712d0dd
grek2 <madisonwharton0@gmail.com>
2025-09-22 15:40:31 -0400

    test

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subsystems/MovementSubsystem.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subsystems/MovementSubsystem.java
@@ -1,6 +1,7 @@
 package org.firstinspires.ftc.teamcode.Subsystems;
 
 import com.arcrobotics.ftclib.command.SubsystemBase;
+import com.arcrobotics.ftclib.drivebase.MecanumDrive;
 import com.arcrobotics.ftclib.hardware.motors.Motor;
 
 public class MovementSubsystem extends SubsystemBase {
@@ -9,6 +10,16 @@ public class MovementSubsystem extends SubsystemBase {
     public Motor frontRightMotor;
     public Motor backLeftMotor;
     public Motor backRightMotor;
+    public MecanumDrive motorControl;
+
+    double speed;
+
+
+    public void driveRobotCentric (double forward, double strafe, double turn) {
+        //do stuff
+    }
+
+
 
 
 }
