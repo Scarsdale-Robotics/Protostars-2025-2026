f6e9565729e089f97e23c7bff3fa72e083f693ed
grek2 <madisonwharton0@gmail.com>
2025-09-22 15:38:59 -0400

    help me

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/TeleOops.java
@@ -0,0 +1,31 @@
+package org.firstinspires.ftc.teamcode.TeleOp;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import org.firstinspires.ftc.teamcode.Subsystems.MovementSubsystem;
+
+public class TeleOops extends LinearOpMode {
+
+
+    public
+
+
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+
+
+
+
+
+    }
+
+    public void DRIVEPLS(){
+        double forward = gamepad1.left_stick_y;
+        double strafe = gamepad1.left_stick_x;
+        double turn = gamepad1.right_stick_y;
+
+
+    }
+
+}
